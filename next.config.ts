97b3faaa2ad5c5571cc22a63614cf5b2df66ca06
João Inez <joaoinez.info@gmail.com>
2025-12-16 23:53:25 +0000

    refactor: follow opennext docs

--- next.config.ts
+++ next.config.ts
@@ -15,3 +15,6 @@ const nextConfig: NextConfig = {
 };
 
 export default withNextIntl(nextConfig);
+
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
